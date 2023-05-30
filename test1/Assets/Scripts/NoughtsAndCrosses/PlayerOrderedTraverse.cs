4219b1890d810d9f3b5a3e94c294e584206eb69e
unknown <haustov@yahoo.com>
2023-05-31 00:19:24 +0400

    Minor performance improve. Added more comments. Improved console logging.

--- test1/Assets/Scripts/NoughtsAndCrosses/PlayerOrderedTraverse.cs
+++ test1/Assets/Scripts/NoughtsAndCrosses/PlayerOrderedTraverse.cs
@@ -33,6 +33,19 @@ namespace NoughtsAndCrosses
             public Score BestNextMoveScore;
         }
 
+        /// <summary>
+        /// Logging to console:
+        /// 0 - nothing
+        /// 1 - just basic information about the result of search
+        /// 2 - improvements of best result (initial screening - only for level 1) and game ends found (wins, losses, draws)
+        /// 3 - tracking dives into new levels and pop outs to upper levels
+        /// 4 - three of traversed branches
+        /// </summary>
+        public int LogLevel = 0;
+
+        /// <summary>
+        /// Moves having heuristic metric below this threshold will not be considered at all
+        /// </summary>
         private const int HeuristicThreshold = 300; // minumal heuristic metric is 208 (a field with no lines (1+1)*100+(4+4))
 
         /// <summary>
@@ -40,6 +53,9 @@ namespace NoughtsAndCrosses
         /// </summary>
         private readonly int TimeLimitTicks;
 
+        /// <summary>
+        /// Traverse stops once this ticks value is reached
+        /// </summary>
         private long HardStopTicks;
 
         private int level;
@@ -62,16 +78,18 @@ namespace NoughtsAndCrosses
             moves_counter = 0;
             Move move = FindBestMove(game);
             long ticks_elapsed = DateTime.Now.Ticks - HardStopTicks + TimeLimitTicks;
-            Console.WriteLine("Ticks elapsed: {0}; Move: X={1},Y={2},Score={3},HeuristicMetric={4}",
-                ticks_elapsed, move.X, move.Y, move.Score, move.HeuristicMetric);
-
-            Console.WriteLine("Moves tried: {0}", moves_counter);
-            /*
-            for (int i=1; i<=MaxDepth; i++)
+            if (LogLevel > 0)
             {
-                Console.WriteLine("Level " + i + ": " + levels[i]);
+                Console.WriteLine("Ticks elapsed: {0}; Move: X={1},Y={2},Score={3},HeuristicMetric={4}", ticks_elapsed, move.X, move.Y, move.Score, move.HeuristicMetric);
+                Console.WriteLine("Moves tried: {0}", moves_counter);
             }
-            */
+            if (LogLevel > 3)
+            {
+                for (int i = 1; i <= MaxDepth; i++)
+                {
+                    Console.WriteLine("Level " + i + ": " + levels[i]);
+                }
+            }
 
             game.Move(move.X, move.Y);
         }
@@ -83,9 +101,12 @@ namespace NoughtsAndCrosses
             Mark[,] board = game.GetBoard();
             Move best = new Move { Score = Score.None, HeuristicMetric = 0 };
             Mark opponent_mark = game.NextMove == Mark.Cross ? Mark.Nought : Mark.Cross;
-            Dictionary<Move, GameBoard> branches = new Dictionary<Move, GameBoard>();
+            //Dictionary<Move, GameBoard> branches = new Dictionary<Move, GameBoard>();
 
-            List<Move> possible_moves = new List<Move>();
+            List<Move> moves_to_view = new List<Move>();
+            List<Move> moves_to_dig = new List<Move>();
+            List<GameBoard> branches_to_dig = new List<GameBoard>();
+
             for (int y = 0; y < game.SizeY; y++)
                 for (int x = 0; x < game.SizeX; x++)
                     if (board[x, y] == Mark.None)
@@ -114,14 +135,14 @@ namespace NoughtsAndCrosses
                         if (best.HeuristicMetric < move.HeuristicMetric)
                         {
                             best = move;
-                            //if (level == 1)
-                            //    Console.WriteLine("Better0: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
+                            if (LogLevel > 1 && level == 1)
+                                Console.WriteLine("Better0: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
                         }
 
                         // if there are no other marks/lines nearby, do not include such field into next stages of search
                         if (metric >= HeuristicThreshold)
                         {
-                            possible_moves.Add(move);
+                            moves_to_view.Add(move);
                         }
                     }
 
@@ -132,18 +153,20 @@ namespace NoughtsAndCrosses
                 best.Y = game.SizeY / 2;
             }
 
-            levels[level] += possible_moves.Count + " ";
+            if (LogLevel > 3)
+            {
+                levels[level] += moves_to_view.Count + " ";
+            }
 
             // Starting traverse from the moves that produce or prevent longest lines (Max)
             // among equally long lines starting from the moves which produce or prevent more lines (Sum)
             // Trying to make a move to each of pre-selected field and see if the game is over and with which result
             // selecting the branch with best result for us (win or draw)
-            // if game is not over with the move, att that "branch of the game" to a list for future consideration
-            foreach (Move move in possible_moves.OrderByDescending(mv => mv.HeuristicMetric))
+            // if game is not over with the move, add that "branch of the game" to a list for future consideration
+            foreach (Move move in moves_to_view.OrderByDescending(mv => mv.HeuristicMetric))
             {
                 GameBoard game_branch = new GameBoard(game);
                 game_branch.Move(move.X, move.Y);
-                //game_branch.PrintBoard(true);
                 if (game_branch.NextMove == Mark.None)
                 {
                     Score score = game_branch.Winner == anticipated_winner ? Score.Win : (game_branch.Winner == GameWinner.Draw ? Score.Draw : Score.Loss);
@@ -151,92 +174,101 @@ namespace NoughtsAndCrosses
                     {
                         best = move;
                         best.Score = score;
-                        //if (level == 1)
-                        //Console.WriteLine("Better1: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
-                        //game_branch.PrintBoard();
+                        if (LogLevel > 1)
+                        {
+                            Console.WriteLine("Better1: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
+                            game_branch.PrintBoard();
+                        }
                     }
                 }
                 else
                 {
-                    branches[move] = game_branch;
+                    moves_to_dig.Add(move);
+                    branches_to_dig.Add(game_branch);
+                    //branches[move] = game_branch;
                 }
             }
 
             // Traversing one by one all game branches where game is not over
             // The traverse might be huge. Limiting it by several parameters: 
-            // 1) traverse depth, or level, or number of moves - our (1) - enemy (2) - our (3) - enemy (4) - etc.
-            // 2) traverse width, number of branches tried on each level
-            // 3) time spent - stopping search when time limit is over
-            // TODO: optimize, use two lists instead of dictionary + ordering of initial list
+            // 1) traverse level (depth, number of moves) - our (1) - enemy (2) - our (3) - enemy (4) - etc.
+            // 2) traverse width (number of branches tried on each level)
+            // 3) time spent - stopping the search when time limit is over and using the best found result
             Score OpponentBestScore = Score.Loss;
-            int branch_count = 0;
             int MaxBr = level <= MaxBranches.Length ? MaxBranches[level - 1] : 2;
-            foreach (Move move in possible_moves.OrderByDescending(mv => mv.HeuristicMetric))
+            if (branches_to_dig.Count < MaxBr)
             {
-                if (branches.ContainsKey(move))
+                MaxBr = branches_to_dig.Count;
+            }
+            for (int branch_count = 0; branch_count < MaxBr; branch_count++)
+            {
+                GameBoard game_branch = branches_to_dig[branch_count];
+                Move move = moves_to_dig[branch_count];
+
+                // If we have time and did not reach depth limit, go into depth, otherwise finish current phase
+                if (DateTime.Now.Ticks < HardStopTicks && level < MaxDepth)
                 {
-                    // limiting width of search tree
-                    if (++branch_count > MaxBr)
+                    if (LogLevel > 2)
                     {
-                        break;
+                        Console.Write(" [L={0} {1}({2},{3})[ ", level, (game.NextMove == Mark.Cross ? 'X' : 'O'), move.X, move.Y);
                     }
-                    GameBoard game_branch = branches[move];
-                    // If we have time and did not reach depth limit, go into depth, otherwise finish current phase
-                    if (DateTime.Now.Ticks < HardStopTicks && level<MaxDepth)
+                    // trying to guess best move of the opponent
+                    Move opponent_best_move = FindBestMove(game_branch);
+
+                    // Calculating best score of the opponent in the branch related to the move to use it as next move score estimation
+                    if (OpponentBestScore < opponent_best_move.Score)
                     {
-                        //Console.Write(" [L={0} {1}({2},{3})[ ", level, (game.NextMove == Mark.Cross ? 'X' : 'O'), move.X, move.Y);
-                        // trying to guess best move of the opponent
-                        Move opponent_best_move = FindBestMove(game_branch);
+                        OpponentBestScore = opponent_best_move.Score;
+                    }
 
-                        // Calculating best score of the opponent in the branch related to the move to use it as next move score estimation
-                        if (OpponentBestScore < opponent_best_move.Score)
+                    if (LogLevel > 2)
+                    {
+                        Console.Write(" ]L={0} {1}({2},{3})] ", level, (game.NextMove == Mark.Cross ? 'X' : 'O'), move.X, move.Y);
+                    }
+
+                    Score score = opponent_best_move.Score == Score.Loss ? Score.Win : (opponent_best_move.Score == Score.Win ? Score.Loss : (opponent_best_move.Score == Score.None ? Score.Unknown : opponent_best_move.Score));
+                    if (LogLevel > 2)
+                    {
+                        Console.WriteLine("best.score={0} X={1} Y={2}, score={3}, level={4}", best.Score, best.X, best.Y, score, level);
+                    }
+                    if (best.Score < score)
+                    {
+                        best = move;
+                        best.Score = score;
+                        best.BestNextMoveScore = opponent_best_move.OpponentBestScore;
+                        best.OpponentHeuristicMetric = opponent_best_move.HeuristicMetric;
+                        if (LogLevel > 1)
                         {
-                            OpponentBestScore = opponent_best_move.Score;
+                            Console.WriteLine("Better2: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
                         }
-
-                        //Console.Write(" ]L={0} {1}({2},{3})] ", level, (game.NextMove == Mark.Cross ? 'X' : 'O'), move.X, move.Y);
-                        /*
-                        Score op_score = opponent_best_move.Score;
-                        Score score = op_score == 
-                            Score.Unknown ? (opponent_best_move.BestNextMoveScore >= Score.OpponentCanWin ? Score.OpponentCanWin : Score.Unknown) : 
-                            ((op_score == Score.Loss || op_score == Score.OpponentCanWin) ? Score.OpponentCanLose : 
-                            ((op_score == Score.Win || op_score == Score.OpponentCanLose) ? Score.OpponentCanWin : Score.OpponentCanDraw));
-                        */
-                        Score score = opponent_best_move.Score == Score.Loss ? Score.Win : (opponent_best_move.Score == Score.Win ? Score.Loss : (opponent_best_move.Score == Score.None ? Score.Unknown : opponent_best_move.Score));
-                        //Console.WriteLine("best.score={0} X={1} Y={2}, score={3}, level={4}", best.Score, best.X, best.Y, score, level);
-                        if (best.Score < score)
+                    }
+                    else if (best.Score == score)
+                    {
+                        // opponent_best_move.OpponentBestScore is best found score of "opponent of opponent", i.e. our best score for next move in that branch
+                        if (best.BestNextMoveScore < opponent_best_move.OpponentBestScore)
                         {
                             best = move;
                             best.Score = score;
                             best.BestNextMoveScore = opponent_best_move.OpponentBestScore;
                             best.OpponentHeuristicMetric = opponent_best_move.HeuristicMetric;
-                            //if (level == 1) 
-                            //Console.WriteLine("Better2: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric,best.OpponentHeuristicMetric);
+                            if (LogLevel > 1)
+                            {
+                                Console.WriteLine("Better3: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
+                            }
                         }
-                        else if (best.Score == score)
+                        else if (best.BestNextMoveScore == opponent_best_move.OpponentBestScore)
                         {
-                            // opponent_best_move.OpponentBestScore is best found score of "opponent of opponent", i.e. our best score for next move in that branch
-                            if (best.BestNextMoveScore < opponent_best_move.OpponentBestScore)
+                            // best.HeuristicMetric cannot be greater than move.HeuristicMetric because of ordering so effectively the condition is "=="
+                            if (best.HeuristicMetric >= move.HeuristicMetric
+                                && (best.OpponentHeuristicMetric == 0 || best.OpponentHeuristicMetric > opponent_best_move.HeuristicMetric))
                             {
                                 best = move;
                                 best.Score = score;
-                                best.BestNextMoveScore = opponent_best_move.OpponentBestScore;
                                 best.OpponentHeuristicMetric = opponent_best_move.HeuristicMetric;
-                                //if (level == 1)
-                                //Console.WriteLine("Better3: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
-                            }
-                            else if (best.BestNextMoveScore == opponent_best_move.OpponentBestScore)
-                            {
-                                // best.HeuristicMetric cannot be greater than move.HeuristicMetric because of ordering so effectively the condition is "=="
-                                if (best.HeuristicMetric >= move.HeuristicMetric
-                                    && (best.OpponentHeuristicMetric == 0 || best.OpponentHeuristicMetric > opponent_best_move.HeuristicMetric))
+                                best.BestNextMoveScore = opponent_best_move.OpponentBestScore;
+                                if (LogLevel > 1)
                                 {
-                                    best = move;
-                                    best.Score = score;
-                                    best.OpponentHeuristicMetric = opponent_best_move.HeuristicMetric;
-                                    best.BestNextMoveScore = opponent_best_move.OpponentBestScore;
-                                    //if (level == 1) 
-                                    //Console.WriteLine("Better4: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
+                                    Console.WriteLine("Better4: level={0},X={1},Y={2},Score={3},BestNextMoveScore={4},HeuristicMetric={5},OpponentHeuristicMetric={6}", level, best.X, best.Y, best.Score, best.BestNextMoveScore, best.HeuristicMetric, best.OpponentHeuristicMetric);
                                 }
                             }
                         }
